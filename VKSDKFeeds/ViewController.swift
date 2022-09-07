3526b4a0e483ef7ae91b5b09c2f7ecc40e88d9f1
insomnia <talkers-maximsOf@icloud.com>
2022-09-07 10:55:44 +0300

    Initial Commit

---
+++ VKSDKFeeds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKSDKFeeds
+//
+//  Created by insomnia on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
